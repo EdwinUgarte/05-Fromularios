b8ef8969fcb321daf540100cbbdb9c1111c9ee5f
EdwinUgarte <Edwin112098>
2023-02-22 16:43:46 -0600

    Add directive at project

---
+++ src/app/template/directives/custom-min.directive.ts
@@ -0,0 +1,31 @@
+import {Directive, Input} from '@angular/core';
+import {Validator,FormControl, NG_VALIDATORS} from '@angular/forms';
+
+
+@Directive({
+     selector: '[customMin][ngModel]',
+     providers: [{
+        provide: NG_VALIDATORS,
+        useExisting: CustomMinDirective,
+        multi: true
+     }]
+})
+export class CustomMinDirective implements Validator{
+
+
+    @Input()
+    minimo!: number;
+
+    constructor(){}
+
+    validate(control: FormControl){
+        const inputValue = control.value;
+      
+        return (inputValue< this.minimo ? {'customMin': true} : null);
+    }
+
+}
+
+
+
+//? Mas informacion de directivas investigar ya que son muy funcionales

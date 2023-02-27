e13dc2a88004d9234355eef4ef911757dd2e816d
EdwinUgarte <Edwin112098>
2023-02-27 14:12:11 -0600

    add Reactive Forms

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styleUrls: ['./switches.component.css']
+  styleUrls: ['./switches.component.css'],
 })
-export class SwitchesComponent {
+export class SwitchesComponent implements OnInit {
+  @ViewChild('miFormulario')
+  miFormulario!: NgForm;
 
+  persona = {
+    genero: '',
+    notificaciones: true,
+  };
+
+  terminos: boolean = false;
+
+  ngOnInit(): void {}
+
+  validarGenero() {
+    if (
+      this.miFormulario?.controls['genero']?.errors
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  validarTerminos() {
+    if (
+      this.miFormulario?.controls['terminos']?.touched &&
+      this.miFormulario?.controls['terminos']?.errors
+    ) {
+      return true;
+    }
+    return false;
+  }
 }

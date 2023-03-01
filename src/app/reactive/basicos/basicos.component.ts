685700e155cce1c86d2121ced7c89ca1ed8173b6
EdwinUgarte <Edwin112098>
2023-03-01 15:53:27 -0600

    add Reactive Forms

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styleUrls: ['./basicos.component.css']
+  styleUrls: ['./basicos.component.css'],
 })
 export class BasicosComponent {
-
   //? Esta es una manera de hacerlo pero se puede ahorrar toda las instancias, creando un FormBuilder como en la parte de abajo
   // miFormulario: FormGroup = new FormGroup({
   //   nombre:     new FormControl('RTX 4080ti'),
@@ -15,24 +19,33 @@ export class BasicosComponent {
   //   existencia: new FormControl(0),
 
   // })
- 
 
-  //? Manera de hacerlo con FormBuilder, es un servicio, NOTA ☝🏻 se tiene que importar en el constructor 
+  //? Manera de hacerlo con FormBuilder, es un servicio, NOTA ☝🏻 se tiene que importar en el constructor
   miFormulario: FormGroup = this.formBuilder.group({
-    nombre:     ['', [Validators.required, Validators.minLength(3)]],
-    precio:     [0, [Validators.required, Validators.min(0)]],
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    precio: [0, [Validators.required, Validators.min(0)]],
     existencia: [0, [Validators.required, Validators.min(0)]],
-  })
+  });
 
+  constructor(private formBuilder: FormBuilder) {}
 
-  constructor(private formBuilder: FormBuilder){}
-
-
-  validarCampo(campo : string){
-    if(this.miFormulario.controls[`${campo}`].errors && this.miFormulario.controls[`${campo}`].touched){
-      return true
+  validarCampo(campo: string) {
+    if (
+      this.miFormulario.controls[`${campo}`].errors &&
+      this.miFormulario.controls[`${campo}`].touched
+    ) {
+      return true;
     }
-    return false
+    return false;
   }
 
+  guardar() {
+    if (this.miFormulario.valid) {
+      this.miFormulario.markAllAsTouched();
+      console.log(this.miFormulario.value);
+      this.miFormulario.reset();
+    }
+
+    return;
+  }
 }
--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styleUrls: ['./dinamicos.component.css']
+  styleUrls: ['./dinamicos.component.css'],
 })
 export class DinamicosComponent {
+
+  
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: ['Edwin', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.formBuilder.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+      ['Spiderman', Validators.required]
+    ]),
+  });
+  
+  nuevoJuego: FormControl = this.formBuilder.control('', Validators.required);
+  
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+
+  
+  constructor(private formBuilder: FormBuilder) {}
+
+  validarCampo(campo: string) {
+    if (
+      this.miFormulario.controls[`${campo}`]?.errors &&
+      this.miFormulario.controls[`${campo}`]?.touched
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  guardar(){
+    if(this.miFormulario.valid){
+      console.log(this.miFormulario.value);
+      this.miFormulario.reset()
+    }
+    else{
+      this.miFormulario.markAllAsTouched();
+      return console.error('Error el formulario no cumple las condiciones');
+    }
+  }
+
+  eliminar(index : number){
+    this.miFormulario.controls['favoritos']?.value.splice(index,1)
+  }
+
+  agregarFavorito(){
+   if(this.nuevoJuego.invalid){return}
+   this.nuevoJuego.reset
+
+  //  this.miFormulario.controls['favoritos']?.value.push( new FormControl(this.nuevoJuego.value).value)
+  this.favoritosArr.push( this.formBuilder.control(this.nuevoJuego.value, Validators.required))
+
+  console.log(this.favoritosArr.controls);
+  }
+
 
 }
